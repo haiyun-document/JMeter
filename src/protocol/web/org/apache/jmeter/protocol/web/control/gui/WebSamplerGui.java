2f8f7cbefb42ba3523963132863f7ce64bc6a705
CP Lim <cheenpin.lim@gmail.com>
2012-04-20 23:47:32 +1000

    Updated with GUI so that behaviour of the browser can be controlled via. Javascript

--- src/protocol/web/org/apache/jmeter/protocol/web/control/gui/WebSamplerGui.java
+++ src/protocol/web/org/apache/jmeter/protocol/web/control/gui/WebSamplerGui.java
@@ -1,12 +1,31 @@
 package org.apache.jmeter.protocol.web.control.gui;
 
+import java.awt.BorderLayout;
+
+import javax.swing.Box;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 import org.apache.jmeter.protocol.web.sampler.WebSampler;
 import org.apache.jmeter.samplers.gui.AbstractSamplerGui;
 import org.apache.jmeter.testelement.TestElement;
+import org.apache.jmeter.util.JMeterUtils;
 
 public class WebSamplerGui extends AbstractSamplerGui {
 
 	private static final long serialVersionUID = -3484685528176139410L;
+
+	private JTextField parameters;// parameters to pass to script file (or
+									// script)
+
+	private JTextArea scriptField;// script area
+
+	public WebSamplerGui() {
+		createGui();
+	}
 
 	@Override
 	public String getLabelResource() {
@@ -16,17 +35,68 @@ public class WebSamplerGui extends AbstractSamplerGui {
 	@Override
 	public TestElement createTestElement() {
 		WebSampler sampler = new WebSampler();
-		configureTestElement(sampler);
-		sampler.setScript("browser.get('http://www.google.com/')");
+		modifyTestElement(sampler);
 		return sampler;
 	}
 
 	@Override
 	public void modifyTestElement(TestElement element) {
-        this.configureTestElement(element);
-        if (!(element instanceof WebSampler)) return;
-        WebSampler sampler = (WebSampler) element;
-		sampler.setScript("browser.get('http://www.google.com/')");
+		element.clear();
+		this.configureTestElement(element);
+		element.setProperty(WebSampler.SCRIPT, scriptField.getText());
+		element.setProperty(WebSampler.PARAMETERS, parameters.getText());
 	}
 
+	private void createGui() {
+		setLayout(new BorderLayout(0, 5));
+		setBorder(makeBorder());
+
+		Box box = Box.createVerticalBox();
+		box.add(makeTitlePanel());
+		box.add(createParameterPanel());
+		add(box, BorderLayout.NORTH);
+
+		JPanel panel = createScriptPanel();
+		add(panel, BorderLayout.CENTER);
+		// Don't let the input field shrink too much
+		add(Box.createVerticalStrut(panel.getPreferredSize().height),
+				BorderLayout.WEST);
+	}
+
+	private JPanel createParameterPanel() {
+		JLabel label = new JLabel(
+				JMeterUtils.getResString("web_script_parameters")); // $NON-NLS-1$
+
+		parameters = new JTextField(10);
+		parameters.setName(WebSampler.PARAMETERS);
+		label.setLabelFor(parameters);
+
+		JPanel parameterPanel = new JPanel(new BorderLayout(5, 0));
+		parameterPanel.add(label, BorderLayout.WEST);
+		parameterPanel.add(parameters, BorderLayout.CENTER);
+		return parameterPanel;
+	}
+
+	private JPanel createScriptPanel() {
+		scriptField = new JTextArea();
+		scriptField.setRows(4);
+		scriptField.setLineWrap(true);
+		scriptField.setWrapStyleWord(true);
+
+		JLabel label = new JLabel(JMeterUtils.getResString("web_script")); // $NON-NLS-1$
+		label.setLabelFor(scriptField);
+
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.add(label, BorderLayout.NORTH);
+		panel.add(new JScrollPane(scriptField), BorderLayout.CENTER);
+
+		JTextArea explain = new JTextArea(
+				JMeterUtils.getResString("web_script_variables")); //$NON-NLS-1$
+		explain.setLineWrap(true);
+		explain.setEditable(false);
+		explain.setBackground(this.getBackground());
+		panel.add(explain, BorderLayout.SOUTH);
+
+		return panel;
+	}
 }
